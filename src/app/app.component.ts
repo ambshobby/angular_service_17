854c2e1ebb5241d9be905f7584a15ca4bbdb4e11
ambshobby <ambs.hobby@gmail.com>
2024-01-31 13:18:24 +0530

    fake store api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostService } from './AppService/post.service';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angularservice_17';
+  apidata:any=[];
+
+  constructor(private service:PostService) {}
+  
+    ngOnInit() {
+  
+     this.caller_api();
+  }
+  caller_api()
+  {
+    this.service.get_APIdata().subscribe((response) => {this.apidata = response;});
+  }
+
 }
